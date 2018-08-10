558d3e35aeab8754af5efa6e981bc81e4c65f753
github username <github email address>
2018-08-10 13:46:21 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
 
   get "students/:id", to: "students#show"
-  get "students/:id/activate", to: "students#activate", as: "activate_student"
+  get "students/:id/activate", to: "students#activate"
 
 
 end
